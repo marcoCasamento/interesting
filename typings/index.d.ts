42c49834ada8f5a25cabd2a0216f011bb7f5d31e
Marco Casamento <marco.casamento@gmail.com>
2016-12-03 15:44:51 +0100

    adding mocha (commit to sync on the laptop)

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/morgan/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="modules/express/index.d.ts" />
